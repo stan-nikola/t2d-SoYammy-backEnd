360f0f98ba0ee62cbf470a00242101299df62103
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2023-04-15 22:03:39 +0300

    tests 2

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const { logInController } = require("./controllers/logInController");
+const {
+  ingredientController,
+} = require("./controllers/ingredient/ingredientController");
 
 require("dotenv").config();
 
@@ -15,6 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/user", logInController);
+app.use("/api/ingredient", ingredientController);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
---
+++ controllers/ingredient/ingredientController.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+
+const ingredientsSchema = Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    avatarURL: String,
+    token: {
+      type: String,
+      default: null,
+    },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const Ingredient = model("ingredient", ingredientsSchema);
+
+const ingredientController = async (req, res) => {
+  const ingredient = await Ingredient.find({});
+
+  res.json(ingredient);
+};
+
+module.exports = { ingredientController };
