7400d7abe888d8ba2cb4a64cc7b7f878c39fe131
Sergey J <aleks.s.educ@gmail.com>
2023-04-19 20:19:13 +0300

    Add login response info

--- controllers/authControllers/userLoginController.js
+++ controllers/authControllers/userLoginController.js
@@ -2,10 +2,12 @@ const { loginUser } = require("../../services");
 
 const userLoginController = async (req, res) => {
   const result = await loginUser(req.body);
-  const { token } = result;
+  const { name, email, token } = result;
   // console.log("token=", token);
   // console.log("result=", result);
   return res.status(200).json({
+    name,
+    email,
     token: token,
   });
 };
